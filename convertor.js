d68ce6a8f4ca447b03e6f47f4ca231900d650201
lavzer <42249680+lavzer@users.noreply.github.com>
2019-12-27 18:43:17 +0300

    Add files via upload

--- convertor.js
+++ convertor.js
@@ -1,3 +1,20 @@
+document.getElementById('theme').onchange = function() {
+    var value = this.options[this.selectedIndex].value;
+    if(value == 0) {
+        body.style.backgroundColor = '#333';
+    } else if(value == 1) {
+        body.style.backgroundColor = 'grey';
+    } else if(value == 2) {
+        body.style.backgroundColor = 'blue';
+    } else {
+        body.style.backgroundColor = 'red';
+    }
+}
+
+
+
+
+
 document.getElementById('outputer').style.visibility = 'hidden';
 
 document.getElementById("select").onchange = function() {
@@ -11,26 +28,25 @@ document.getElementById("select").onchange = function() {
     }
 }
 
-
 document.getElementById('inputer').addEventListener('input', function(e) {
     var value = document.getElementById('select').options[document.getElementById('select').selectedIndex].value;
     if(value == '0') {
         document.getElementById('outputer').style.visibility = 'visible';
         let km = e.target.value;
-        document.getElementById('firstOutput').innerHTML = km * 1000 + 'm';
-        document.getElementById('secondOutput').innerHTML = km * 100000 + 'sm';
-        document.getElementById('thirdOutput').innerHTML = km * 1000000 + 'mm';
+        document.getElementById('firstOutput').innerHTML = km * 1000 + ' Метров';
+        document.getElementById('secondOutput').innerHTML = km * 100000 + ' Сантиметров';
+        document.getElementById('thirdOutput').innerHTML = km * 1000000 + ' Миллиметров';
     } else if(value == '1') {
         document.getElementById('outputer').style.visibility = 'visible';
         let kg = e.target.value;
-        document.getElementById('firstOutput').innerHTML = kg / 1000 + 't';
-        document.getElementById('secondOutput').innerHTML = kg * 1000 + 'g';
-        document.getElementById('thirdOutput').innerHTML = kg * 1000000 + 'mg';
+        document.getElementById('firstOutput').innerHTML = kg / 1000 + ' Тонн';
+        document.getElementById('secondOutput').innerHTML = kg * 1000 + ' Килограмм';
+        document.getElementById('thirdOutput').innerHTML = kg * 1000000 + 'Миллиграмм';
     } else {
         document.getElementById('outputer').style.visibility = 'visible';
         let kmh = e.target.value;
-        document.getElementById('firstOutput').innerHTML = kmh * 1000 + 'm/s';
-        document.getElementById('secondOutput').innerHTML = kmh * 100000 + 'm/h';
-        document.getElementById('thirdOutput').innerHTML = kmh * 1000000 + 'km/s';
+        document.getElementById('firstOutput').innerHTML = kmh * 1000 / 3600 + ' Метров в секунду';
+        document.getElementById('secondOutput').innerHTML = kmh * 1000 + ' Метров в час';
+        document.getElementById('thirdOutput').innerHTML = kmh / 3600 + ' Километров в час';
     }
 });

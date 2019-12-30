0cb0ff074b923695aa7de52543a452047f2b09cc
lavzer <42249680+lavzer@users.noreply.github.com>
2019-12-30 18:02:50 +0300

    Add files via upload

--- convertor.js
+++ convertor.js
@@ -36,17 +36,26 @@ document.getElementById('inputer').addEventListener('input', function(e) {
         document.getElementById('firstOutput').innerHTML = km * 1000 + ' Метров';
         document.getElementById('secondOutput').innerHTML = km * 100000 + ' Сантиметров';
         document.getElementById('thirdOutput').innerHTML = km * 1000000 + ' Миллиметров';
+        if(km == 0) {
+            document.getElementById('outputer').style.visibility = 'hidden';
+        }
     } else if(value == '1') {
         document.getElementById('outputer').style.visibility = 'visible';
         let kg = e.target.value;
         document.getElementById('firstOutput').innerHTML = kg / 1000 + ' Тонн';
         document.getElementById('secondOutput').innerHTML = kg * 1000 + ' Килограмм';
         document.getElementById('thirdOutput').innerHTML = kg * 1000000 + 'Миллиграмм';
+        if(kg == 0) {
+            document.getElementById('outputer').style.visibility = 'hidden';
+        }
     } else {
         document.getElementById('outputer').style.visibility = 'visible';
         let kmh = e.target.value;
         document.getElementById('firstOutput').innerHTML = kmh * 1000 / 3600 + ' Метров в секунду';
         document.getElementById('secondOutput').innerHTML = kmh * 1000 + ' Метров в час';
         document.getElementById('thirdOutput').innerHTML = kmh / 3600 + ' Километров в час';
+        if(kmh == 0) {
+            document.getElementById('outputer').style.visibility = 'hidden';
+        }
     }
 });
